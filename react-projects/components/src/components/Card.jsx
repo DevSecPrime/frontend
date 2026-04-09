a7ae1be1bea72c0a96a79560daa59f38531bc99c
Aryan Rana <ranaaryan620@gmail.com>
2026-04-09 21:48:08 +0530

    feat: initialize React project with Vite, add components and styles
    - Created a new React project using Vite.
    - Added main application structure with App component.
    - Implemented Navbar and Card components.
    - Included SVG icons for social media and documentation.
    - Added CSS styles for layout and theming.
    - Configured Vite for development and build processes.

---
+++ react-projects/components/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h1> This is Card1 Component</h1>
+    </div>
+  );
+};
+
+export default Card;
---
+++ react-projects/components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <main>This is my Navbar</main>
+    </div>
+  );
+};
+
+export default Navbar;
