ae3e27bf8e08e866b35f876d2709d9f2da592379
Aryan Rana <ranaaryan620@gmail.com>
2026-04-09 23:48:45 +0530

    Add initial project setup with React, Vite, and basic styles
    - Created Card component structure.
    - Added index.css for global styles.
    - Set up main entry point with ReactDOM.
    - Configured Vite for React project.
    - Added SVG assets for React and Vite logos.

---
+++ react-projects/card-props-project/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      {/* Wrapper div  */}
+      <div>
+        {/* share icon */}
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Card;
