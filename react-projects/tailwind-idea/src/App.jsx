7b4b8cb27114cfc4a473bfd581a2a54fa7b67d0a
Aryan Rana <ranaaryan620@gmail.com>
2026-04-11 20:12:02 +0530

    feat: initialize React project with Tailwind CSS and add SVG assets
    - Added react.svg and vite.svg assets to the project.
    - Created index.css to import Tailwind CSS styles.
    - Set up main.jsx as the entry point for the React application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ react-projects/tailwind-idea/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-violet-300 h-screen w-full text-center text-2xl flex items-center justify-center text-grey-800 font-bold">
+        {" "}
+        Hello, Tailwind CSS!
+      </h1>
+    </>
+  );
+}
+
+export default App;
