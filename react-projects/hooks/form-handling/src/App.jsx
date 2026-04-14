fc2dfa792c482305117cf5cf0c99aba6d89efb00
Aryan Rana <ranaaryan620@gmail.com>
2026-04-14 23:25:11 +0530

    feat: implement username input handling in App component

--- react-projects/hooks/form-handling/src/App.jsx
+++ react-projects/hooks/form-handling/src/App.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 
 const App = () => {
+  const [userName, setUserName] = useState("")
+
+  // function changeHandler(e) {
+  //   console.log("change handler called")
+  //   console.log("event: ", e.target.value)
+  //   e.preventDefault()
+  //   setUserName(e.target.value)
+  // }
+
   function submitHandler(event) {
     event.preventDefault()
-    console.log("event: ", event)
+    setUserName("")
 
   }
   return (
     <div>
       <form action="" className="me-app">
-        <input type="text" placeholder="Username" />
+        <input
+          type="text"
+          placeholder="Username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)} />
         <button type="submit" onClick={(event) => submitHandler(event)}>Login</button>
       </form>
     </div>
