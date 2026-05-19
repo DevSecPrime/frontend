19750c6cb2e5e810faabb258ed5f104e694179fa
DevSecPrime <ranaaryan620@gmail.com>
2026-05-19 20:53:17 +0530

    feat: integrate axios for media fetching and implement search functionality with Redux

--- react-projects/redux/redux-gallery/src/App.jsx
+++ react-projects/redux/redux-gallery/src/App.jsx
@@ -1,5 +1,22 @@
+import { getImages, getVideos } from "./api/mediaApi";
+
 const App = () => {
-  return <div className="h-screen w-full bg-gray-900 text-white">App</div>;
+  return (
+    <div className="h-screen w-full bg-gray-900 text-white">
+      <button
+        onClick={async () => await getImages()}
+        className="bg-green-400 py-3 px-4 m-5 text-black"
+      >
+        Get images
+      </button>
+      <button
+        onClick={async () => await getVideos()}
+        className="bg-green-400 py-3 px-4 m-5 text-black"
+      >
+        Get videos
+      </button>
+    </div>
+  );
 };
 
 export default App;
---
+++ react-projects/redux/redux-gallery/src/redux/features/searchSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  query: "",
+  activeTab: "photos",
+  results: [],
+  loading: false,
+  error: null,
+};
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setQuery(state, action) {
+      state.query = action.payload;
+    },
+    setActiveTab(state, action) {
+      state.activeTab = action.payload;
+    },
+    setResults(state, action) {
+      state.results = action.payload;
+      state.loading = false; // if we have got the results off the loading
+    },
+    setLoading(state) {
+      state.loading = true;
+      state.error = null; // when there is loading there should not be any error
+    },
+    setError(state, action) {
+      state.setError = action.payload;
+      state.loading = false; // set loading false when there is an error
+    },
+  },
+});
+
+export const { setQuery, setActiveTab, setResults, setLoading, setError } =
+  searchSlice.actions;
+
+export default searchSlice.reducer;
