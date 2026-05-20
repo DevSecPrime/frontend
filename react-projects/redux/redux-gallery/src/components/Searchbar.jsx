a37ac2c676719410da51847c05ecd423a2f028f5
DevSecPrime <ranaaryan620@gmail.com>
2026-05-20 21:04:42 +0530

    feat: enhance Searchbar component with styling and layout improvements

--- react-projects/redux/redux-gallery/src/components/Searchbar.jsx
+++ react-projects/redux/redux-gallery/src/components/Searchbar.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const Searchbar = () => {
-  const [inputText, setInputText] = useState("");
+  // const [inputText, setInputText] = useState("");
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Search anything..." />
+      <form className="flex bg-gray-900 gap-5 py-10 px-14">
+        <input
+          type="text"
+          placeholder="Search anything..."
+          required
+          className="w-full border-2 px-6 py-3 text-xl"
+        />
         <button>Search</button>
       </form>
     </div>
